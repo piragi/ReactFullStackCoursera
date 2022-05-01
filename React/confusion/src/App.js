7653291f652fd360acecd941fb3d7e30581d8234
Julius Šula <julius.sula@gmail.com>
2022-05-01 18:25:54 +0200

    components part 2

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -4,8 +4,18 @@ import { Navbar } from 'reactstrap'
 import './App.css';
 import { NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
     render() {
         return ( 
             <div>
@@ -14,7 +24,7 @@ class App extends Component {
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
-                <Menu/>
+                <Menu dishes={this.state.dishes}/>
             </div>
         );
     }
