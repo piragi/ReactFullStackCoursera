f34af0760575f6a0fe9659da8463b7eb24ee06a5
Julius Šula <julius.sula@gmail.com>
2022-06-20 13:05:51 +0200

    Passport and small bugfixes

--- NodeJS/node-express/conFusionServer/app.js
+++ NodeJS/node-express/conFusionServer/app.js
@@ -14,6 +14,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
@@ -47,26 +50,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
